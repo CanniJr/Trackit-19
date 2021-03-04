b66342d624b9e7b60b9aa7d5cd578606923420e6
Cannigia Laluw <cannigialaluw@gmail.com>
2021-03-03 22:52:09 -0500

    Add dropdown list from API fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { MenuItem, FormControl, Select } from '@material-ui/core'
 import './App.css';
 
+// API endpoint: https://disease.sh/v3/covid-19/countries
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide')
+  
+  useEffect(() => {
+    // async function
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((resp) => resp.json())
+      .then((data) => {
+        const countriesData = data.map((countryObj) => ({
+          name: countryObj.country,
+          info: countryObj.countryInfo.iso2,
+        }));
+        setCountries(countriesData)
+      });
+    }
+    getCountriesData();
+  }, [])
+
+  const changeHandler = (e) => {
+    const countryCode = e.target.value;
+    setCountry(countryCode)
+  }
+
   return (
-   <div>
-     <h1>Let's Track it!</h1>
+   <div className='app'>
+      <div className='app__header'>
+      <h1>Let's Track Covid-19 !</h1>
+        <FormControl className='app__dropdown'>
+          <Select variant='outlined' onChange={changeHandler} value={country}>
+            <MenuItem value='worldwide'>Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.info}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+        
+      {/*Infobox */}
+      {/*Infobox */}
+      {/*Infobox */}
+    
+      {/*Table */}
+      {/*Graph */}
+      {/*Map */}
    </div>
   );
 }
